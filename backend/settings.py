a7d5f2e77a7e46f17078331b351ba947f21e83e9
okeyogrey <greyokeyo@gmail.com>
2025-03-11 22:45:38 +0300

    Step 3: Configured PostgreSQL database

--- backend/settings.py
+++ backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kosc_db',
+        'USER': 'kosc_user',
+        'PASSWORD': 'grey7956',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
