67fb36b90bf3dd8dbbda76d2e8b517a523dd80f4
okeyogrey <greyokeyo@gmail.com>
2025-03-11 22:02:22 +0300

    Step 2: Created core app with Product and Category models

---
+++ core/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
